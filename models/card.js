6fb83641de9453f5dc2e73aae27c2cdb37022a78
Mikhail Zinchik <MVMGroup888@yandex.ru>
2023-08-26 18:30:29 +0300

    fix schemas

--- models/card.js
+++ models/card.js
@@ -4,17 +4,13 @@ const mongoose = require('mongoose');
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'Поле "name" должно быть заполнено'],
-    minlength: [2, 'Минимальная длина поля "name" - 2'],
-    maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    required: true,
+    minlength: 2,
+    maxlength: 30,
   },
   link: {
     type: String,
-    required: [true, 'Поле "link" должно быть заполнено'],
-    // validate: {
-    //   validator: (v) => validator.isURL(v),
-    //   message: 'Некорректный URL',
-    // },
+    required: true,
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
