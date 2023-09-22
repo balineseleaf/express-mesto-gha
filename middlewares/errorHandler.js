5cd772c2e1b8a6ed36ff9764f893cee4bb85d5ff
Mikhail Zinchik <MVMGroup888@yandex.ru>
2023-09-22 17:30:27 +0300

    add console.dir

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 function errorHandler(err, req, res, next) { // принято обрабатывать ошибки централизованно.
   // Для этого нужно добавить приложению мидлвэр такого вида:
   const { statusCode = 500, message } = err;
-
+  console.dir(err);
   res
     .status(statusCode)
     .send({
